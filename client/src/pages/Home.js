409af0448366a72ad41678493fc30ba2be167abe
rakib3004 <bsse1129@iit.du.ac.bd>
2023-02-24 08:42:03 +0600

    complete home.js

---
+++ client/src/pages/Home.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="mb-20">
+      <h1 className="sm:text-4xl text-2xl font-blod my-6 text-gray-900">
+        Hello, This is my first research blog
+      </h1>
+      <p className='mx-auto leading-relaxed text-base mb-4'>
+        Officia nulla et nostrud tempor commodo excepteur anim nisi incididunt
+        fugiat. Minim id ut excepteur officia ut adipisicing non. Consequat et
+        eiusmod reprehenderit velit pariatur. Elit exercitation esse cupidatat
+        nulla Lorem. Aliqua minim culpa excepteur tempor fugiat consectetur
+        Lorem. Consequat incididunt incididunt non exercitation fugiat et nisi
+        incididunt. Pariatur incididunt mollit esse id sunt ullamco aliqua
+        proident. Aute ex elit irure eiusmod voluptate incididunt est.
+        Adipisicing do tempor dolore ex magna in culpa adipisicing anim aliqua
+        labore. Ullamco consectetur pariatur labore proident labore esse.
+        Laboris culpa duis eu laborum adipisicing dolore magna eiusmod quis
+        Lorem eu mollit Lorem labore. Quis id pariatur labore veniam veniam
+        consequat anim id elit ad quis mollit. Eiusmod qui labore excepteur ex
+        do excepteur reprehenderit ex esse ea.
+      </p>
+      <p className='mx-auto leading-relaxed text-base mb-4'>
+        Officia nulla et nostrud tempor commodo excepteur anim nisi incididunt
+        fugiat. Minim id ut excepteur officia ut adipisicing non. Consequat et
+        eiusmod reprehenderit velit pariatur. Elit exercitation esse cupidatat
+        nulla Lorem. Aliqua minim culpa excepteur tempor fugiat consectetur
+        Lorem. Consequat incididunt incididunt non exercitation fugiat et nisi
+        incididunt. Pariatur incididunt mollit esse id sunt ullamco aliqua
+        proident. Aute ex elit irure eiusmod voluptate incididunt est.
+        Adipisicing do tempor dolore ex magna in culpa adipisicing anim aliqua
+        labore. Ullamco consectetur pariatur labore proident labore esse.
+        Laboris culpa duis eu laborum adipisicing dolore magna eiusmod quis
+        Lorem eu mollit Lorem labore. Quis id pariatur labore veniam veniam
+        consequat anim id elit ad quis mollit. Eiusmod qui labore excepteur ex
+        do excepteur reprehenderit ex esse ea.
+      </p>
+      <p className='mx-auto leading-relaxed text-base mb-4'>
+        Officia nulla et nostrud tempor commodo excepteur anim nisi incididunt
+        fugiat. Minim id ut excepteur officia ut adipisicing non. Consequat et
+        eiusmod reprehenderit velit pariatur. Elit exercitation esse cupidatat
+        nulla Lorem. Aliqua minim culpa excepteur tempor fugiat consectetur
+        Lorem. Consequat incididunt incididunt non exercitation fugiat et nisi
+        incididunt. Pariatur incididunt mollit esse id sunt ullamco aliqua
+        proident. Aute ex elit irure eiusmod voluptate incididunt est.
+        Adipisicing do tempor dolore ex magna in culpa adipisicing anim aliqua
+        labore. Ullamco consectetur pariatur labore proident labore esse.
+        Laboris culpa duis eu laborum adipisicing dolore magna eiusmod quis
+        Lorem eu mollit Lorem labore. Quis id pariatur labore veniam veniam
+        consequat anim id elit ad quis mollit. Eiusmod qui labore excepteur ex
+        do excepteur reprehenderit ex esse ea.
+      </p>
+    </div>
+  );
+};
+
+export default Home;

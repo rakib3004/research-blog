ca8028c3cf70d89cec5acd9a48880a184db262c5
rakib3004 <bsse1129@iit.du.ac.bd>
2023-02-24 10:45:48 +0600

    failed to find error

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,18 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home"
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
-//Pages
-import Home from "./pages/Home";
-
-function App() {
+const App = () => {
   return (
-   /* <Router>
-      <div className="max-w-screen-md mx-auto pt-20">
-        <Routes>
-          <Route path=' /' element={<Home /> } />
-        </Routes>
-      </div>
-    </Router>
+    
+   <Router>
      <div className="max-w-screen-md mx-auto pt-20">
-    <Home/>
-  </div>*/
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      </Routes>        
+    </div>
+   </Router>
 
-  <Router>
-<div className="max-w-screen-md mx-auto pt-20">
-  <Routes>
-    <Route path="/" element={<Home/>} />
-  </Routes>
-</div>
-  </Router>
-  
-  );
+    )
 }
 
-export default App;
+export default App

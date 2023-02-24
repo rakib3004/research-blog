4d8ecc202fbce5f5975f7694833bebcb1e043524
rakib3004 <bsse1129@iit.du.ac.bd>
2023-02-24 14:38:51 +0600

    add new about article and articles list page in this project

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,9 @@
 import Home from "./pages/Home"
+import About from "./pages/About";
+import ArticlesList  from "./pages/ArticlesList";
+import Article from "./pages/Article";
+
+
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 const App = () => {
@@ -8,6 +13,10 @@ const App = () => {
      <div className="max-w-screen-md mx-auto pt-20">
     <Routes>
       <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/articles-list" element={<ArticlesList/>}/>
+      <Route path="/article" element={<Article/>}/>
+
       </Routes>        
     </div>
    </Router>
---
+++ client/src/pages/Article.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Article = () => {
+  return (
+    <div>
+      <h1 className="sm:text-4xl text-2xl font-blod my-6 text-gray-900">
+        This is a article
+      </h1>
+    </div>
+  );
+};
+
+export default Article;

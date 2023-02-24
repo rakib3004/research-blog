f3a6e896be1bbf6bff82e6fb782735ba2810b667
rakib3004 <bsse1129@iit.du.ac.bd>
2023-02-24 16:19:42 +0600

    complete show articles in article list page

--- client/src/pages/ArticlesList.js
+++ client/src/pages/ArticlesList.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import articleContent from "./article-content";
 
 const ArticlesList = () => {
   return (
-    <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">
-      Articles
-    </h1>
-  )
-}
+    <div>
+      <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">
+        Articles
+      </h1>
+
+      <div className="container py-4 mx-auto">
+        <div className="flex flex-wrap -m-4">
+          {articleContent.map((article, index) => (
+         <div key={index} className="p-4 md:w-1/2">
+            <div className="h-full border-2 border-gray-200 border-opacity-60 rounde-lg overflow-hidden">
+              <Link to={`/article/${article.name}`}>
+                <img
+                  className="lg:h-48 md:h-36 w-full object-cover object-center"
+                  src={article.thumbnail}
+                  alt={article.title}
+                ></img>
+              </Link>
+            </div>
+             </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ArticlesList;

5e544891e4f34a83b49c1a3c256f5558e1d1bde2
rakib3004 <bsse1129@iit.du.ac.bd>
2023-03-03 14:49:19 +0600

    capitalize content

--- client/src/pages/article-content.js
+++ client/src/pages/article-content.js
@@ -14,7 +14,7 @@ const articles = [
     title: "Introduction to Git and Github",
     thumbnail: "/images/learn-github.jpg",
     content: [
-      `git add is a command used to add a file that is in the working directory to the staging area.
+      `Git add is a command used to add a file that is in the working directory to the staging area.
       git commit is a command used to add all files that are staged to the local repository.
       git push is a command used to add all committed files in the local repository to the remote repository. So in the remote repository, all files and changes will be visible to anyone with access to the remote repository.
       git fetch is a command used to get files from the remote repository to the local repository but not into the working directory.

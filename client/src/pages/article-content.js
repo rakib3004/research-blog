191eb3313cb5e5a9b5de42bb178e7027909ccb32
rakib3004 <bsse1129@iit.du.ac.bd>
2023-03-03 14:48:08 +0600

    add new blog

--- client/src/pages/article-content.js
+++ client/src/pages/article-content.js
@@ -5,9 +5,7 @@ const articles = [
     thumbnail: "/images/learn-python.jpg",
     content: [
       `Hello folks, Python is one of the best programming languages in the tech world and a great tool if you want to become a Software Developer, Data Scientist, Web Developer, or Machine Learning Engineer.
-
     Because of its ever-increasing popularity, more and more people are learning Python but at the same time, they are also struggling to find the best resources to learn Python in a guided and structured manner.
-    
     Being an author of a technical blog, I often receive queries like how do I learn Python, what is the best way to learn Python? Could you please suggest the best resource to learn Python, or should I join an online course from Udemy or Coursera to learn Python or not, etc?`,
     ],
   },
@@ -29,10 +27,28 @@ const articles = [
     title: "React Crash Course",
     thumbnail: "/images/learn-react.jpg",
     content: [
-      `There is a ton of options when it comes to client-side frameworks, and often it can be a little confusing for beginners to choose from a wide range of choices. Often these choices boil down to Angular, React, and Vue. “So, which is the best choice?”, you may ask. I can’t help but give you the clichéd answer that there isn’t really the best choice. Just pick one and learn it thoroughly, would be the best advice to give, because all the three choices eventually boil down to very similar working strategies.
+      `There is a ton of options when it comes to client-side frameworks, and often it can be a little confusing for beginners to choose from a wide range of choices.
+       Often these choices boil down to Angular, React, and Vue. “So, which is the best choice?”, you may ask. I can’t help but give you the clichéd answer that there
+       isn’t really the best choice. Just pick one and learn it thoroughly, would be the best advice to give, because all the three choices eventually boil down to very
+        similar working strategies.
 
       `,
     ],
+  },
+  {
+    name: "learn-http",
+    title: "HTTP In Depth",
+    thumbnail: "/images/learn-http.jpg",
+    content: [
+      `The Hypertext Transfer Protocol (HTTP) is an application protocol for distributed, collaborative, and hypermedia information systems.
+      HTTP is the foundation of data communication for the World Wide Web.
+      Protocola protocol is the special set of rules that end points in a 
+      telecommunication connection use when they communicate. Protocols specify interactions between the communicating entities.
+      Protocols usually consist of three main parts:-      
+      1- Header: contains information such as source, destination, fileType and size.
+      2- Payload: the actual information transmitted using the protocol .
+      3- Footer: a control field to ensure the Payload data is transmitted free of errors.`
+    ]
   },
 ];
 

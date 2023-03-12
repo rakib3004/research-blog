1420ba5b26a1800dd26a1479f1f634501666b333
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2023-03-12 13:02:40 +0600

    connect with database in reasearch blog is complete

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const {MongoClient} = require('mongodb');
 const PORT = process.env.PORT || 8000;
 
 
@@ -20,6 +21,20 @@ const articlesInfo = {
 // body parser is now built in express.json
 
 app.use(express.json({extended: false}));
+
+app.get("/api/articles/:name", async (req,res)=>{
+    try{
+        const articleName = req.params.name;
+        const client = await MongoClient.connect("mongodb://localhost:27017");
+        const db = client.db("research-blog");
+        const articleInfo = await db.collection("articles").findOne({name: articleName});
+        res.status(200).json(articleInfo);
+        client.close();
+    }
+    catch(err){
+        res.status(500).send({message: "Error connecting to database", err})
+    }
+})
 
 app.post("/api/articles/:name/add-comments", (req,res)=>{
 
